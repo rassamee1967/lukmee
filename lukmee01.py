3cec9cf3d5a5c88a50667657154a149a0fecf822
rassamee1967 <164286@wsk.ac.th>
2026-06-24 02:53:54 +0000

    lukmee01.py

---
+++ lukmee01.py
@@ -0,0 +1,7 @@
+print("โปรแกรมคำนวณหาพื้นที่สามเหลี่ยม")
+print("--------------------------")
+tall = float(input("enter gieght:"))
+base = float(input("enter base:"))
+areatri = 0.5*tall*base
+print("พื้นที่สามเหลี่ยม=",areatri)
+print("ผู้พัฒนาโปรแกรม โดยนางสาว รัศมี แสงคำ")
